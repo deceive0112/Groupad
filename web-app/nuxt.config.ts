cad413242326e491d13e0b24a7a9d14c62fa4c07
deceive0112 <94655133+deceive0112@users.noreply.github.com>
2026-03-08 15:21:21 +0800

    Begin new Groupad project

---
+++ web-app/nuxt.config.ts
@@ -0,0 +1,23 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite";
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true },
+  css: ['./app/assets/css/main.css'],
+
+  modules: [
+    '@nuxt/eslint',
+    '@nuxt/icon',
+    '@nuxt/image',
+    'shadcn-nuxt'
+  ],
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
+  shadcn: {
+    prefix: '',
+    componentDir: '@/components/ui'
+  }
+})
